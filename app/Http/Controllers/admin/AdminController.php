58fdaf23cdfa7423cb44003db68c464a3b67d336
romeo <romeo.lenizo@student.passerellesnumeriques.org>
2021-01-23 22:08:14 +0800

    01-23-00

--- app/Http/Controllers/admin/AdminController.php
+++ app/Http/Controllers/admin/AdminController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\PublishedStory;
 
 class AdminController extends Controller
 {
     public function getStory()
     {
-        
+        return response()->json(PublishedStory::all());
     }
 }
